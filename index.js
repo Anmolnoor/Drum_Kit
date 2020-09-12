030513c1353ec33900a2da2223dd7fcaa86516c0
Anmol Noor <anmolnoor59@gmail.com>
2020-09-12 20:38:49 +0530

    All Done

---
+++ index.js
@@ -0,0 +1,52 @@
+var n = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < n; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {    var i = this.innerHTML;    check(i); buttonAnimation(i);  });
+}
+document.addEventListener("keydown",function(event){check(event.key); buttonAnimation(event.key)}) ;
+
+
+
+function check(word){
+  switch (word) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    default:
+
+        break;
+  }
+}
+
+function buttonAnimation(word){
+  document.querySelector("."+word).classList.add("pressed");
+  setTimeout(function() {
+    document.querySelector("."+word).classList.remove("pressed");
+  },100)
+}
